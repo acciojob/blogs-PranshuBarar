ad411abe96029d234d6801d5c6085731908c7082
91894@PRANSHU <pranshubarar1851996@gmail.com>
2023-02-18 15:07:38 +0530

    Solution5

--- src/main/java/com/driver/controller/BlogController.java
+++ src/main/java/com/driver/controller/BlogController.java
@@ -17,7 +17,7 @@ public class BlogController {
     BlogService blogService;
 
     @PostMapping
-    public ResponseEntity createBlog(@RequestParam Integer userId, @RequestParam String title, @RequestParam String content) {
+    public ResponseEntity createBlog(@RequestParam Integer userId, @RequestParam String title, @RequestParam String content) throws Exception{
         // Create a blog and add it under given user
         blogService.createAndReturnBlog(userId, title, content);
         return new ResponseEntity<>(HttpStatus.CREATED);
--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -6,6 +6,7 @@ import com.driver.models.User;
 import com.driver.repositories.BlogRepository;
 import com.driver.repositories.ImageRepository;
 import com.driver.repositories.UserRepository;
+import com.sun.istack.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +22,12 @@ public class BlogService {
     @Autowired
     UserRepository userRepository1;
 
-    public Blog createAndReturnBlog(Integer userId, String title, String content) {
+    public Blog createAndReturnBlog(Integer userId, String title, String content) throws Exception {
         //create a blog at the current time
+
+        if(!userRepository1.findById(userId).isPresent()){
+            throw new Exception();
+        }
         User user = userRepository1.findById(userId).get();
         Blog blog = new Blog(user,title,content);
         blogRepository1.save(blog);
